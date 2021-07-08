08dc1e2fb5cb41c742e526720ef477cdf36878ca
Alexandre Carelli <alexandre.carelli@gmail.com>
2021-07-08 12:55:10 -0400

    starting home page & authentication

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -15,11 +15,11 @@ export function Home() {
       <main>
         <div className="main-content">
           <img src={logoImg} alt="Letmeask" />
-          <button>
+          <button className="create-room">
             <img src={googleIconImg} alt="Google" />
             Crie sua sala com o Google
           </button>
-          <div>ou entre em uma sala</div>
+          <div className="separator">ou entre em uma sala</div>
           <form>
             <input
               type="text"
